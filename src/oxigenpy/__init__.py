a807a3367a06047a5f6396f03c243e93acbfd752
Anselmo Blanco Dominguez <anselmo.colab@anatel.gov.br>
2026-01-20 21:30:34 -0300

    expondo __version__

--- src/oxigenpy/__init__.py
+++ src/oxigenpy/__init__.py
@@ -2,6 +2,10 @@
 oxigenpy - utilitários Python da Oxigenai
 """
 
+from importlib.metadata import version as _version
+
+__version__ = _version("oxigenpy")
+
 from . import strings
 
-__all__ = ["strings"]
+__all__ = ["strings", "__version__"]
